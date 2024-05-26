ca43bcc0ec57af9f54248111031bffb6d3870c7a
Ariana Bavand <you@example.com>
2024-05-26 15:50:16 +0300

    database errors handeled final

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    mongodb_username: 'admin',
-    mongodb_password: 'Ariana24we',
-    mongodb_clustername: 'cluster0',
-    mongodb_database: 'auth',
-  },
 };
 
 export default nextConfig;
